4cadd00a005c14090d1404d5be968b61feb323a5
ASUS <1139395629@qq.com>
2021-03-31 16:47:23 +0800

    功能--音轨变量注释补充

--- src/main/java/entity/GlobalVariablesEntity.java
+++ src/main/java/entity/GlobalVariablesEntity.java
@@ -8,12 +8,28 @@ import lombok.Data;
 @Data
 public class GlobalVariablesEntity {
 
+    /**
+     * 音符间隙
+     * L = 8，那就代表默认音符为8分之一音符。L默认 = 4
+     */
     private Integer L;
 
+    /**
+     * 音域
+     * 初始化时，音域为中央C，即 O = 4。此参数会随><o三个参数浮动
+     */
     private Integer O;
 
+    /**
+     * BPM
+     * 一分钟多少拍，默认为 BPM = 120
+     */
     private Integer T;
 
+    /**
+     * 音量
+     * 原神无此参数
+     */
     private Integer V;
 
     public GlobalVariablesEntity() {
